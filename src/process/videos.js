57875037e826ee40ca3249b1242910b0e98cb135
Nikolay Ivanov <mraiur@gmail.com>
2018-05-14 11:08:23 +0300

    Skip processing webm videos nad directly write the metadata file

--- src/process/videos.js
+++ src/process/videos.js
@@ -8,6 +8,42 @@ let files = fs.readdirSync(VideosDir, {});
 let skipped = 0;
 let processed = 0;
 
+function writeMetaFile( converteFileName, convertedFilePath, fileMetaPath)
+{
+    let fileMeta = {
+            "name" : converteFileName,
+            "file": converteFileName,
+            "src" : "videos/"+converteFileName,
+            "type" : 1,
+            "rbgList" : [],
+            "_duration": "",
+            "duration" : 0
+        };
+
+    ffmpeg.ffprobe( convertedFilePath ,function(err, metadata) {
+        let format = metadata.format;
+        fileMeta.duration = Math.ceil(format.duration);
+        fileMeta._duration = prettyMs( fileMeta.duration * 1000);
+
+        let randomDuration = 0;
+        let step = 0;
+        while( randomDuration < fileMeta.duration )
+        {
+            step = Math.floor(Math.random() * 25) + 5;
+            fileMeta.rbgList.push({
+                r : Math.floor(Math.random() * 256),
+                g : Math.floor(Math.random() * 256),
+                b : Math.floor(Math.random() * 256),
+                stepTime: step,
+                startTime: randomDuration
+            });
+            randomDuration+= step;
+        }
+
+        fs.writeFileSync(fileMetaPath, JSON.stringify(fileMeta, null, 4));
+    });
+}
+
 files.forEach( ( file ) =>
 {
     let filePath = VideosDir + '/' + file ;
@@ -22,51 +58,54 @@ files.forEach( ( file ) =>
 
         if( fs.existsSync( fileMetaPath ) === false )
         {
-            let fileMeta = {
-                "name" : converteFileName,
-                "file": converteFileName,
-                "src" : "videos/"+converteFileName,
-                "type" : 1,
-                "rbgList" : [],
-                "_duration": "",
-                "duration" : 0
-            };
             console.log("file: ", file);
-            ffmpeg(filePath)
-                .size("1024x?")
-                .videoBitrate('1024k')
-                .videoCodec('libvpx-vp9')
-                .on('start', function(commandLine) {
-                    console.log('Spawned Ffmpeg with command: ' + commandLine);
-                })
-                .on('error', function(err) {
-                    console.log("error", err);
-                })
-                .on('end', function(){
-                    ffmpeg.ffprobe( convertedFilePath ,function(err, metadata) {
-                        let format = metadata.format;
-                        fileMeta.duration = Math.ceil(format.duration);
-                        fileMeta._duration = prettyMs( fileMeta.duration * 1000);
 
-                        let randomDuration = 0;
-                        let step = 0;
-                        while( randomDuration < fileMeta.duration )
-                        {
-                            step = Math.floor(Math.random() * 25) + 5;
-                            fileMeta.rbgList.push({
-                                r : Math.floor(Math.random() * 256),
-                                g : Math.floor(Math.random() * 256),
-                                b : Math.floor(Math.random() * 256),
-                                stepTime: step,
-                                startTime: randomDuration
-                            });
-                            randomDuration+= step;
-                        }
+            if(/\.webm/.exec(file))
+            {
+                writeMetaFile(converteFileName, convertedFilePath, fileMetaPath);
+            }
+            else
+            {
+                ffmpeg(filePath)
+                    .size("1024x?")
+                    .videoBitrate('1024k')
+                    .videoCodec('libvpx-vp9')
+                    .on('start', function(commandLine) {
+                        console.log('Spawned Ffmpeg with command: ' + commandLine);
+                    })
+                    .on('error', function(err) {
+                        console.log("error", err);
+                    })
+                    .on('end', function(){
+                        writeMetaFile(converteFileName, convertedFilePath, fileMetaPath);
+                        /*ffmpeg.ffprobe( convertedFilePath ,function(err, metadata) {
+                            let format = metadata.format;
+                            fileMeta.duration = Math.ceil(format.duration);
+                            fileMeta._duration = prettyMs( fileMeta.duration * 1000);
 
-                        fs.writeFileSync(fileMetaPath, JSON.stringify(fileMeta, null, 4));
-                    });
-                })
-                .save(convertedFilePath);
+                            let randomDuration = 0;
+                            let step = 0;
+                            while( randomDuration < fileMeta.duration )
+                            {
+                                step = Math.floor(Math.random() * 25) + 5;
+                                fileMeta.rbgList.push({
+                                    r : Math.floor(Math.random() * 256),
+                                    g : Math.floor(Math.random() * 256),
+                                    b : Math.floor(Math.random() * 256),
+                                    stepTime: step,
+                                    startTime: randomDuration
+                                });
+                                randomDuration+= step;
+                            }
+
+                            fs.writeFileSync(fileMetaPath, JSON.stringify(fileMeta, null, 4));
+                        });*/
+                    })
+                    .save(convertedFilePath);
+            }
+            
+            
+            
         }
         else
         {
