7135b5256ae4fb3f43d743d10a1f19c8fc9c298e
Nikolay Ivanov <mraiur@gmail.com>
2019-11-05 00:04:16 +0200

    feat-game-mode: refactor RGB signaling
    - RGB mixin shared between player and games

---
+++ src/client/js/mixins/Rgb.js
@@ -0,0 +1,29 @@
+export default {
+	components: {},
+	methods: {
+		rgbClear(){
+			this.rgb(255,255,255);
+		},
+		timedRgb( r, g, b, t){
+			this.rgb(r,g,b);
+			setTimeout(this.rgbClear, t * 1000 );
+		},
+		rgbSequence( list )
+		{
+			return this.$http.post('http://localhost:3030/rgb/sequence', list, {})
+				.then(resp => {
+					console.log("rgb response", resp);
+				});
+		},
+		rgb( r, g, b )
+		{
+
+			return this.$http.post('http://localhost:3030/rgb', { r: r, g: g, b: b}, {
+				emulateJSON: true
+			})
+				.then(resp => {
+					console.log("rgb response", resp);
+				});
+		},
+	}
+}
